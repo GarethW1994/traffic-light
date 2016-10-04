b51dac23ff602fdab62b7a1e0d07c6a2399c9ac3
GarethW1994 <williams.gareth0@gmail.com>
2016-10-04 10:28:08 +0200

    Save changes made to functions

--- conditional-logic.js
+++ conditional-logic.js
@@ -4,12 +4,12 @@ function captureTrafficLightChange(number){
     //write your code here!;
     // number is the value entered in the text on the screen
    if (number > 40) {
-      go();
+      stop();
     }
     else if (number >= 30 && number <= 40) {
       caution();
     }
     else {
-      stop();
-    } 
+      go();
+    }
 };
--- set-interval.js
+++ set-interval.js
@@ -1,28 +1,35 @@
 var counter = 0;
 var number;
 
-function showNumber(num){
+function showNumber(num) {
     if (!number){
         number = document.querySelector("#number");
     }
     number.innerHTML = counter;
 }
 
-function interval(){
+function interval() {
         counter++;
-        showNumber(counter);
         // write some code here
-        if (counter > 0) {
-          go();
-        }
-        else if (counter >= 30 && counter <= 40) {
-          caution();
-        }
-        else {
-          stop();
-        }
-        };
 
-}
+  if (counter === 60)
+  {
+    counter = 0;
+  }
+
+  if (counter > 40) {
+    go();
+  }
+
+  if (counter > 29 < 40) {
+    caution();
+  }
+
+  if (counter < 29) {
+    stop();
+  }
+      showNumber(counter);
+      }
+
 
 setInterval(interval, 500);
